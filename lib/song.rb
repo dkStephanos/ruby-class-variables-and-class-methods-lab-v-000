5f90190153a3eb8745a83989a456efce323a3f14
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-31 18:12:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,13 +10,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@count += 1
-    if !artists.include?(artist)
-      @@artists << artist
-    end
-    if !genres.include?(genre)
-      @@genres << genre
-    end
+    
   end
 
   def self.count
